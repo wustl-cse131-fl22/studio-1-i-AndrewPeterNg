622a3e742d5214c9221cffcb31373df3a2e71029
andrewng <andrewng@localhost>
2022-09-08 12:41:31 -0500

    Studio One Complete

---
+++ src/studio1/LeapYear.java
@@ -0,0 +1,16 @@
+package studio1;
+
+import java.util.Scanner;
+
+public class LeapYear {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.println("Value for year?");
+		int year = in.nextInt();
+		boolean leapyear = year%4 == 0 && year%100 > 0 || year%400 == 0;
+		System.out.print(leapyear);
+	}
+
+}
